2d63dd60cff6c862947d83be3aeb05396765ece3
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2016-11-20 15:23:19 +0100

    Edit

--- lab3/zad5.c
+++ lab3/zad5.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-        int a,b;
-        printf("Podaj dwie liczby:\n");
-        scanf("%d %d",&a,&b);
-        int suma=0;
-        while (a>=1)
-        {
-          if (a%2!=0) suma=suma+b;
-          a=a>>1;
-          b=b<<1;
-        }
-        printf("Wynik mnozenia: %d\n",suma);
-        return 0;
+    int a,b;
+    printf("Podaj dwie liczby:\n");
+    scanf("%d %d",&a,&b);
+    int suma=0;
+    while (a>=1)
+    {
+        if (a%2!=0) suma=suma+b;
+        a=a>>1;
+        b=b<<1;
+    }
+    printf("Wynik mnozenia: %d\n",suma);
+    return 0;
 }
