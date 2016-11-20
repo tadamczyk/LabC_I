f7263c6e0a298a229b6834a6cb9c288bb94c822a
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2016-11-20 15:22:10 +0100

    Edit

--- lab3/zad5.c
+++ lab3/zad5.c
@@ -7,9 +7,9 @@ int main()
         int suma=0;
         while (a>=1)
         {
-        if (a%2!=0) suma=suma+b;
-        a=a>>1;
-        b=b<<1;
+          if (a%2!=0) suma=suma+b;
+          a=a>>1;
+          b=b<<1;
         }
         printf("Wynik mnozenia: %d\n",suma);
         return 0;
